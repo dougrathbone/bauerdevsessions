9b0f18fbd7fd5f780f8318bc6ffe46a098758d31
Antony Francis <anfrancis@bauer-media.com.au>
2013-12-09 16:41:41 +1100

    added angular js demo

---
+++ 2013.12.December/bauerdevsession.angularjs/BauerDevSession.AngularJS/Controllers/DefaultController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BauerDevSession.AngularJS.Controllers
+{
+    public class DefaultController : Controller
+    {
+        //
+        // GET: /Default/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HelloWorld()
+        {
+            return View();
+        }
+
+        public ActionResult GiftRequest()
+        {
+            return View();
+        }
+
+        public JsonResult GetListOfGifts()
+        {
+            var gifts = new Gift[] {new Gift {Code = "PS4", Name = "Play Station 4"}, new Gift {Code = "X1", Name = "XBox One"}};
+            
+            return Json(gifts, JsonRequestBehavior.AllowGet);
+        }
+	}
+
+    class Gift
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
